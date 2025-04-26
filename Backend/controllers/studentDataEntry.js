26c05d422829234ac7a3fd6475dda09fda06efc2
Sulayman <developer.sulayman@gmail.com>
2025-04-26 19:17:16 +0600

    edited

--- Backend/controllers/studentDataEntry.js
+++ Backend/controllers/studentDataEntry.js
@@ -1,11 +1,10 @@
 
 export const studentDataEntry = (req, res) => {
     try {
-      console.log("hello")
+      
       res.status(200).json({ message: "Student data entry successful" })
     } catch (e) {
       console.error("error", e)
       res.status(500).json({ message: "Server error", error: e.message })
-    }
   }
   
