ceb75d8f79e5f8b304607fb47ce453f02088b2cb
Sulayman <developer.sulayman@gmail.com>
2025-04-26 19:18:23 +0600

    bug fixed

--- Backend/controllers/studentDataEntry.js
+++ Backend/controllers/studentDataEntry.js
@@ -1,10 +1,10 @@
 
 export const studentDataEntry = (req, res) => {
     try {
-      
+      console.log()
       res.status(200).json({ message: "Student data entry successful" })
     } catch (e) {
       console.error("error", e)
       res.status(500).json({ message: "Server error", error: e.message })
   }
-  
+};
